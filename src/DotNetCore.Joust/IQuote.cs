9eac18a168f295d52d9c51711fb0c82c39f534dd
Dan Nixon <nixon.daniel.j@gmail.com>
2016-11-18 14:35:01 -0500

    commented IQuote properties

--- src/DotNetCore.Joust/IQuote.cs
+++ src/DotNetCore.Joust/IQuote.cs
@@ -1,5 +1,14 @@
 public interface IQuote
 {
+    // Total price including material cost, labor cost, and margin
     float Price { get; }
+    
+    // Cost of all carpet orders from suppliers
+    float MaterialCost {get;}
+
+    // Total cost of installation labor
+    float LaborCost {get;}
+
+    // Inventory IDs of all rolls of carpet to be purchased
     string[] RollOrders {get;set;}
 }
