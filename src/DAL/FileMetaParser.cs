6f4c384678430b945fb26aa0a0577f725229e58b
ksmurph1 <ksmurph1@gmail.com>
2016-12-04 17:26:59 -0600

    metadata

--- src/DAL/FileMetaParser.cs
+++ src/DAL/FileMetaParser.cs
@@ -1,6 +1,8 @@
 using System.Xml;
 using System.IO;
-using System.Configuration
+using System.Configuration;
+using System.Threading.Task;
+using System.Generic.Collections;
 
 namespace DAL
 {
@@ -24,9 +26,31 @@ namespace DAL
             };
             using (XmlReader parser=new XmlReader(new StreamReader(filename),options))
             {
+                parser.MoveToContent();
+                parser.ReadStartElement("file-descriptor");
+                bool status=parser.ReadToDescendant("column");
+            while (status)
+            {
+                Task<Tuple<byte,string,Type>> t=new Task<Tuple<byte,string,Type>>(ParseColumnData, parser.ReadSubTree());
+                t.ContinueWith((ant)=>MetaData.Add(ant.Item1,ant.Item2, ant.Item3),
+                                 TaskContinuationOptions.RunContinuationsAsynchronously);
+                t.Start();
+                status=parser.ReadToNextSibling("column");
+            } 
                 
             }
                   
+        }
+
+        private static Tuple<byte,string,Type> ParseColumnData(XmlReader reader)
+        {
+                   reader.Read(); // move to element
+                   byte index=Convert.ToByte(reader.GetAttribute("id"));
+                   reader.ReadToFollowing("name");
+                   string colname=reader.ReadContentAsString();
+                   reader.ReadToNextSibling("data-type");
+                   Type t=Type.GetType(reader.ReadContentAsString());
+                   return new Tuple<byte,string,Type>(index,colname,t);
         }
     }
 }
--- src/DAL/MetaData.cs
+++ src/DAL/MetaData.cs
@@ -1,29 +1,37 @@
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
 namespace DAL
 {
     public static class MetaData
     {
-        private static List<string> columnNames;
-        private static List<Type> columnTypes;
+        private static ConcurrentBag<KeyValuePair<byte,string>> columnNames;
+        private static ConcurrentBag<KeyValuePair<byte,Type>> columnTypes;
 
-        public static void addColumn(string name, Type colType)
+        public static void addColumn(byte index, string name, Type colType)
         {
-            columnNames.Add(name);
-            columnTypes.Add(colType);
+            columnNames.Add(new KeyValuePair<byte,string>(index, name));
+            columnTypes.Add(new KeyValuePair<byte,Type>(index, colType));
         }
 
         public static string[] getColumnNames()
         {
-            return columnNames.ToArray();
+            string[] names= new string[columnNames.Count];
+            KeyValuePair<byte,string> result;
+            while (!columnNames.IsEmpty)
+            {
+                if (columnNames.TryTake(out result))
+                {
+                    names[result.Key]=result.Value;
+                }
+            }
+            return names;
         }
 
         public static Type getColumnType(string colName)
         {
-            Type result;
-            int index=columnNames.FindIndex(s=>s==colName);
-            if (index >= 0)
-               result=columnTypes[index];
-            else
-               result=typeof(Util.Null);
+            int index=columnNames.First(kp=>kp.Value == colName).Select(kp=>kp.Key).DefaultIfEmpty(-1);
+            Type result=columnTypes.Where(kp=>kp.Key == index).Select(kp=>kp.Value).DefaultIfEmpty(typeof(Util.Null));
             return result;
         }
     }
