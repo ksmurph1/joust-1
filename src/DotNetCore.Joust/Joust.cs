518fdb309dfee79bdd9f6b47013840d44b8ebd08
Kerry Murphy <ksmurph1@gmail.com>
2016-12-15 21:55:55 -0600

    working on Joust

--- src/DotNetCore.Joust/Joust.cs
+++ src/DotNetCore.Joust/Joust.cs
@@ -6,16 +6,16 @@ namespace Joust
 {
     public sealed class LowestCostCalculator : IJoust
     {
-        private List<IInventory> inventory = new List<IInventory>();
+        private List<IInventory> suppliers = new List<IInventory>();
         //0,sqftreq 1,roomcount 2,hourlylabor 3,desiredgrade
-         private enum InputNames: byte {SQFTREQ=0, ROOMCNT, HRLYLABOR, DESGRADE};
+         private enum InputNames: byte {SQFTREQ=0, ROOMCNT, HRLYLABOR, GRADE};
 
          public LowestCostCalculator()
          {
              ValueReturnObj<IInventory> status=CompanyInventory.CreateInstance();
              while(status.Value != null && status.Exception == null)
              {
-                inventory.Add(status.Value);
+                suppliers.Add(status.Value);
                 status=CompanyInventory.CreateInstance();
              } 
              if (status.Exception != null)
@@ -26,7 +26,11 @@ namespace Joust
          }
          public IQuote GetQuote(int[] input)
          {
-                input[(byte)InputNames.DESGRADE]
+             foreach (IInventory supplier in suppliers)
+             {
+                supplier.ApplyOperation(d=>d.Price/(double)(d.Width*d.Length),
+                d=>d.Grade >= input[(byte)InputNames.GRADE]);
+             }
          }
     }
 
